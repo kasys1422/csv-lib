49cad451f4012127d7c4099e2e6d8337282b0693
kasys1422 <ka.sys.1422@gmail.com>
2022-06-30 13:40:40 +0900

    Update csv-lib.h

--- csv-lib.h
+++ csv-lib.h
@@ -1,10 +1,10 @@
 //////////////////////////////////////////////////////
-// CSV read header-only library (C++)				//
-// (JP)CSV読み込みヘッダーオンリーライブラリ(C++)	//
-// version 0.0.2									//
-// Copyright(c) 2022 kasys1422						//
-// This software is released under the MIT License.	//
-// http://opensource.org/licenses/mit-license.php	//
+// CSV read header-only library (C++)               //
+// (JP)CSV読み込みヘッダーオンリーライブラリ（C++） //
+// version 0.0.2                                    //
+// Copyright(c) 2022 kasys1422                      //
+// This software is released under the MIT License. //
+// http://opensource.org/licenses/mit-license.php   //
 //////////////////////////////////////////////////////
 
 #pragma once
