7f9eb76521ea6fb8ab3034c36a4e287ffeb5c9fc
kasys1422 <ka.sys.1422@gmail.com>
2022-07-03 06:55:00 +0900

    update license

--- csv-lib.h
+++ csv-lib.h
@@ -2,8 +2,6 @@
 // CSV read header library (C++)                    //
 // version 0.0.2                                    //
 // Copyright(c) 2022 kasys1422                      //
-// This software is released under the MIT License. //
-// http://opensource.org/licenses/mit-license.php   //
 //////////////////////////////////////////////////////
 
 #pragma once
